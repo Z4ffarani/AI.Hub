918643468dc818a7a5e1fa714c981a7fd8d305a5
Z4ffarani <zaffarani.kaique@gmail.com>
2024-09-06 04:20:12 -0300

    unify

--- paginas/dados.js
+++ paginas/dados.js
@@ -2,31 +2,31 @@ const dadosIA = {
     "assistente": [
         {
             titulo: "Google Gemini",
-            imagem: "./assets/google-gemini.png",
+            imagem: "./assets-descubra/google-gemini.png",
             descricao: "Google Gemini é um chatbot avançado da Google que oferece suporte para uma variedade de tarefas.",
             link: "https://www.google.com/gemini"
         },
         {
             titulo: "ChatGPT",
-            imagem: "./assets/chatgpt.png",
+            imagem: "./assets-descubra/chatgpt.png",
             descricao: "ChatGPT é um chatbot da OpenAI que oferece suporte em conversas naturais e auxilia em diversas tarefas.",
             link: "https://chat.openai.com"
         },
         {
             titulo: "Microsoft Copilot",
-            imagem: "./assets/microsoft-copilot.png",
+            imagem: "./assets-descubra/microsoft-copilot.png",
             descricao: "Microsoft Copilot é uma ferramenta de assistente virtual da Microsoft que integra capacidades de IA em vários produtos.",
             link: "https://www.microsoft.com/copilot"
         },
         {
             titulo: "Cohesive AI",
-            imagem: "./assets/cohesive-ai.jpg",
+            imagem: "./assets-descubra/cohesive-ai.jpg",
             descricao: "Cohesive é uma ferramenta de assistente virtual que oferece suporte em várias áreas, como organização e produtividade.",
             link: "https://cohesive.so"
         },
         {
             titulo: "Meta AI",
-            imagem: "./assets/meta-ai.png",
+            imagem: "./assets-descubra/meta-ai.png",
             descricao: "Meta AI é uma plataforma de assistente virtual desenvolvida pela Meta, focada em integração e suporte avançado.",
             link: "https://www.meta.ai/"
         },
@@ -34,31 +34,31 @@ const dadosIA = {
     "design": [
         {
             titulo: "Stable Diffusion",
-            imagem: "./assets/stable-diffusion.png",
+            imagem: "./assets-descubra/stable-diffusion.png",
             descricao: "Stable Diffusion é uma IA que gera imagens de alta qualidade a partir de descrições textuais.",
             link: "https://stablediffusionweb.com"
         },
         {
             titulo: "Looka",
-            imagem: "./assets/looka.png",
+            imagem: "./assets-descubra/looka.png",
             descricao: "Looka usa IA para criar logotipos e identidades visuais personalizadas para marcas.",
             link: "https://looka.com"
         },
         {
             titulo: "Dreamstudio",
-            imagem: "./assets/dreamstudio.png",
+            imagem: "./assets-descubra/dreamstudio.png",
             descricao: "Dreamstudio é uma plataforma de design que usa IA para gerar imagens e arte digital.",
             link: "https://dreamstudio.ai"
         },
         {
             titulo: "Fotor",
-            imagem: "./assets/fotor.png",
+            imagem: "./assets-descubra/fotor.png",
             descricao: "Fotor é uma ferramenta de edição de fotos e design gráfico que usa IA para melhorar imagens.",
             link: "https://www.fotor.com"
         },
         {
             titulo: "Deep Dream Generator",
-            imagem: "./assets/deep-dream-generator.png",
+            imagem: "./assets-descubra/deep-dream-generator.png",
             descricao: "Deep Dream Generator usa IA para transformar imagens comuns em obras de arte surrealistas.",
             link: "https://deepdreamgenerator.com"
         },
@@ -66,31 +66,31 @@ const dadosIA = {
     "video": [
         {
             titulo: "KlingAI",
-            imagem: "./assets/kling-ai.jpg",
+            imagem: "./assets-descubra/kling-ai.jpg",
             descricao: "KlingAI utiliza IA para gerar vídeos e edições automatizadas com base em dados.",
             link: "https://klingai.com"
         },
         {
             titulo: "Ssemble",
-            imagem: "./assets/ssemble.png",
+            imagem: "./assets-descubra/ssemble.png",
             descricao: "Ssemble usa IA para ajudar na criação e edição de vídeos com suporte automatizado.",
             link: "https://ssemble.com"
         },
         {
             titulo: "InVideo",
-            imagem: "./assets/invideo.png",
+            imagem: "./assets-descubra/invideo.png",
             descricao: "InVideo oferece criação de vídeos automatizada a partir de texto e modelos, utilizando IA.",
             link: "https://invideo.io"
         },
         {
             titulo: "ElevenLabs",
-            imagem: "./assets/eleven-labs.png",
+            imagem: "./assets-descubra/eleven-labs.png",
             descricao: "ElevenLabs utiliza IA para criar e editar vídeos com recursos avançados de análise e criação.",
             link: "https://www.elevenlabs.io"
         },
         {
             titulo: "Falatron",
-            imagem: "./assets/falatron.png",
+            imagem: "./assets-descubra/falatron.png",
             descricao: "Falatron é uma IA que cria vídeos e conteúdos visuais com base em análises de dados e texto.",
             link: "https://falatron.com"
         },
@@ -98,31 +98,31 @@ const dadosIA = {
     "musica": [
         {
             titulo: "Suno AI",
-            imagem: "./assets/suno-ai.png",
+            imagem: "./assets-descubra/suno-ai.png",
             descricao: "Suno AI utiliza IA para compor música com base em parâmetros definidos pelo usuário.",
             link: "https://suno.ai"
         },
         {
             titulo: "Udio",
-            imagem: "./assets/udio.png",
+            imagem: "./assets-descubra/udio.png",
             descricao: "Udio usa IA para criar música personalizada e adaptar composições às necessidades do usuário.",
             link: "https://udio.com"
         },
         {
             titulo: "Mubert",
-            imagem: "./assets/mubert.png",
+            imagem: "./assets-descubra/mubert.png",
             descricao: "Mubert utiliza IA para gerar música em tempo real para diferentes propósitos e ambientes.",
             link: "https://mubert.com"
         },
         {
             titulo: "Soundraw",
-            imagem: "./assets/soundraw.png",
+            imagem: "./assets-descubra/soundraw.png",
             descricao: "Soundraw é uma IA que cria músicas personalizadas com base nas preferências e necessidades do usuário.",
             link: "https://soundraw.io"
         },
         {
             titulo: "Aiva",
-            imagem: "./assets/aiva.png",
+            imagem: "./assets-descubra/aiva.png",
             descricao: "Aiva é uma IA compositora que cria músicas originais em diversos estilos e gêneros.",
             link: "https://www.aiva.ai"
         },
@@ -130,31 +130,31 @@ const dadosIA = {
     "estudo": [
         {
             titulo: "Perplexity",
-            imagem: "./assets/perplexity.png",
+            imagem: "./assets-descubra/perplexity.png",
             descricao: "Perplexity é uma IA que fornece respostas e análises detalhadas para questões acadêmicas e pesquisas.",
             link: "https://www.perplexity.ai"
         },
         {
             titulo: "Humata",
-            imagem: "./assets/humata.png",
+            imagem: "./assets-descubra/humata.png",
             descricao: "Humata é uma ferramenta de IA que auxilia na compreensão e análise de textos complexos.",
             link: "https://www.humata.ai"
         },
         {
             titulo: "Kuki.ia",
-            imagem: "./assets/kuki-ai.png",
+            imagem: "./assets-descubra/kuki-ai.png",
             descricao: "Kuki.ia é uma IA que fornece explicações e análises sobre uma ampla gama de tópicos acadêmicos e técnicos.",
             link: "https://www.kuki.ai/"
         },
         {
             titulo: "Consensus",
-            imagem: "./assets/consensus.png",
+            imagem: "./assets-descubra/consensus.png",
             descricao: "Consensus utiliza IA para fornecer resumos e respostas baseadas em consenso de várias fontes.",
             link: "https://consensus.app"
         },
         {
             titulo: "Typeset",
-            imagem: "./assets/typeset.png",
+            imagem: "./assets-descubra/typeset.png",
             descricao: "Typeset usa IA para formatar e melhorar a apresentação de documentos acadêmicos e textos.",
             link: "https://www.typeset.io"
         },
