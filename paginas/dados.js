5aadc24e245bf3152699da4dc86750117462e805
Z4ffarani <zaffarani.kaique@gmail.com>
2024-10-17 22:24:14 -0300

    light images

--- paginas/dados.js
+++ paginas/dados.js
@@ -2,31 +2,31 @@ const dadosIA = {
     "geral": [
         {
             titulo: "Google Gemini",
-            imagem: "./assets-descubra/google-gemini.png",
+            imagem: "./assets-descubra/google-gemini.webp",
             descricao: "Google Gemini é um chatbot avançado que oferece suporte versátil para uma ampla gama de tarefas e interações.",
             link: "https://www.google.com/gemini"
         },
         {
             titulo: "ChatGPT",
-            imagem: "./assets-descubra/chatgpt.png",
+            imagem: "./assets-descubra/chatgpt.webp",
             descricao: "ChatGPT é um chatbot da OpenAI que fornece interações conversacionais e assistência em diversas atividades.",
             link: "https://chat.openai.com"
         },
         {
             titulo: "Copilot",
-            imagem: "./assets-descubra/microsoft-copilot.png",
+            imagem: "./assets-descubra/microsoft-copilot.webp",
             descricao: "Copilot é um assistente virtual da Microsoft que integra IA em produtos para melhorar produtividade e suporte.",
             link: "https://www.microsoft.com/copilot"
         },
         {
             titulo: "Meta AI",
-            imagem: "./assets-descubra/metaai.png",
+            imagem: "./assets-descubra/metaai.webp",
             descricao: "Meta AI é um chatbot da Meta que combina raciocínio complexo, visualização de ideias e resolução de problemas.",
             link: "https://www.meta.ai"
         },
         {
             titulo: "Jasper",
-            imagem: "./assets-descubra/jasper.png",
+            imagem: "./assets-descubra/jasper.webp",
             descricao: "Jasper é um chatbot de IA especializado em criar textos e conteúdo de marketing a partir de prompts específicos.",
             link: "https://www.jasper.ai"
         },
@@ -38,7 +38,7 @@ const dadosIA = {
         },
         {
             titulo: "Gamma",
-            imagem: "./assets-descubra/gamma.png",
+            imagem: "./assets-descubra/gamma.webp",
             descricao: "Gamma é uma plataforma de criação de apresentações e documentos que utiliza IA para facilitar o design e a formatação.",
             link: "https://gamma.app"
         },
@@ -46,43 +46,43 @@ const dadosIA = {
     "imagem": [
         {
             titulo: "Adobe Firefly",
-            imagem: "./assets-descubra/adobe-firefly.png",
+            imagem: "./assets-descubra/adobe-firefly.webp",
             descricao: "O Adobe Firefly é uma plataforma da Adobe projetada para criar conteúdo visual de maneira rápida e intuitiva com IA generativa.",
             link: "https://firefly.adobe.com/?ff_channel=adobe_com&ff_campaign=feature_page&ff_source=firefly_seo"
         },
         {
             titulo: "Midjourney",
-            imagem: "./assets-descubra/midjourney.png",
+            imagem: "./assets-descubra/midjourney.webp",
             descricao: "MidJourney gera imagens a partir de descrições textuais, proporcionando resultados visuais criativos e únicos.",
             link: "https://www.midjourney.com/explore?tab=top"
         },
         {
             titulo: "NightCafe",
-            imagem: "./assets-descubra/nightcafeai.png",
+            imagem: "./assets-descubra/nightcafeai.webp",
             descricao: "NightCafe cria imagens a partir de prompts e estilos diversos, permitindo uma ampla personalização visual.",
             link: "https://creator.nightcafe.studio"
         },
         {
             titulo: "Stable Diffusion",
-            imagem: "./assets-descubra/stable-diffusion.png",
+            imagem: "./assets-descubra/stable-diffusion.webp",
             descricao: "Stable Diffusion gera imagens de alta qualidade a partir de descrições textuais, com ênfase em detalhes e precisão.",
             link: "https://stablediffusionweb.com"
         },
         {
             titulo: "Krea.ai",
-            imagem: "./assets-descubra/krea.ai.png",
+            imagem: "./assets-descubra/krea.ai.webp",
             descricao: "Krea.ai é uma plataforma de criação de imagens e vídeos gerados por IA, ajustando-os com estilos e temas.",
             link: "https://www.krea.ai/home"
         },
         {
             titulo: "Dreamstudio",
-            imagem: "./assets-descubra/dreamstudio.png",
+            imagem: "./assets-descubra/dreamstudio.webp",
             descricao: "Dreamstudio utiliza IA para criar arte digital e imagens com base em inputs criativos dos usuários.",
             link: "https://dreamstudio.ai"
         },
         {
             titulo: "Looka",
-            imagem: "./assets-descubra/looka.png",
+            imagem: "./assets-descubra/looka.webp",
             descricao: "Looka gera logotipos e identidades visuais personalizadas utilizando IA para atender às necessidades de marcas e empresas.",
             link: "https://looka.com"
         },
@@ -90,13 +90,13 @@ const dadosIA = {
     "video": [
         {
             titulo: "Runway",
-            imagem: "./assets-descubra/runway.png",
+            imagem: "./assets-descubra/runway.webp",
             descricao: "Runway é uma plataforma de IA para criação e edição de vídeos, com ferramentas inovadoras para diversos tipos de conteúdo.",
             link: "https://runwayml.com"
         },
         {
             titulo: "Luma AI",
-            imagem: "./assets-descubra/lumaai.png",
+            imagem: "./assets-descubra/lumaai.webp",
             descricao: "Luma AI usa IA para gerar vídeos e conteúdos visuais a partir de prompts e imagens, facilitando a criação de mídia personalizada.",
             link: "https://lumalabs.ai/dream-machine"
         },
@@ -108,19 +108,19 @@ const dadosIA = {
         },
         {
             titulo: "InVideo",
-            imagem: "./assets-descubra/invideo.png",
+            imagem: "./assets-descubra/invideo.webp",
             descricao: "InVideo fornece criação automatizada de vídeos a partir de promptss e modelos, ideal para marketing e produção rápida.",
             link: "https://invideo.io"
         },
         {
             titulo: "Ssemble",
-            imagem: "./assets-descubra/ssemble.png",
+            imagem: "./assets-descubra/ssemble.webp",
             descricao: "Ssemble utiliza IA para facilitar a criação e edição de vídeos, com suporte automatizado para otimizar o processo.",
             link: "https://ssemble.com"
         },
         {
             titulo: "Lumen5",
-            imagem: "./assets-descubra/lumen5.png",
+            imagem: "./assets-descubra/lumen5.webp",
             descricao: "Lumen5 transforma prompts em vídeos de marketing profissionais, utilizando IA para criar conteúdo visual de alta qualidade.",
             link: "https://lumen5.com"
         },
@@ -128,37 +128,37 @@ const dadosIA = {
     "audio": [
         {
             titulo: "ElevenLabs",
-            imagem: "./assets-descubra/eleven-labs.png",
+            imagem: "./assets-descubra/eleven-labs.webp",
             descricao: "ElevenLabs cria vozes realistas a partir de prompt, ideal para narrações e dublagens com alta qualidade de áudio.",
             link: "https://www.elevenlabs.io"
         },
         {
             titulo: "Suno AI",
-            imagem: "./assets-descubra/suno-ai.png",
+            imagem: "./assets-descubra/suno-ai.webp",
             descricao: "Suno AI compõe músicas com base em parâmetros definidos pelo usuário, proporcionando áudio personalizado.",
             link: "https://suno.ai"
         },
         {
             titulo: "Udio",
-            imagem: "./assets-descubra/udio.png",
+            imagem: "./assets-descubra/udio.webp",
             descricao: "Udio gera música personalizada e adapta composições conforme as necessidades do usuário, utilizando IA.",
             link: "https://udio.com"
         },
         {
             titulo: "Mubert",
-            imagem: "./assets-descubra/mubert.png",
+            imagem: "./assets-descubra/mubert.webp",
             descricao: "Mubert cria música em tempo real para diversos usos, aplicando IA para ajustar o áudio conforme o ambiente.",
             link: "https://mubert.com"
         },
         {
             titulo: "Soundraw",
-            imagem: "./assets-descubra/soundraw.png",
+            imagem: "./assets-descubra/soundraw.webp",
             descricao: "Soundraw oferece criação de músicas personalizadas baseadas nas preferências do usuário, com suporte de IA.",
             link: "https://soundraw.io"
         },
         {
             titulo: "Aiva",
-            imagem: "./assets-descubra/aiva.png",
+            imagem: "./assets-descubra/aiva.webp",
             descricao: "Aiva compõe músicas originais em diversos estilos e gêneros, utilizando IA para criar trilhas sonoras personalizadas.",
             link: "https://www.aiva.ai"
         },
@@ -166,31 +166,31 @@ const dadosIA = {
     "estudo": [
         {
             titulo: "Humata",
-            imagem: "./assets-descubra/humata.png",
+            imagem: "./assets-descubra/humata.webp",
             descricao: "Humata ajuda na compreensão e análise de textos complexos, utilizando IA para facilitar a pesquisa e aprendizado.",
             link: "https://www.humata.ai"
         },
         {
             titulo: "Claude",
-            imagem: "./assets-descubra/claude.png",
+            imagem: "./assets-descubra/claude.webp",
             descricao: "Claude é um assistente de IA que gera textos e fornece suporte em diversas atividades acadêmicas e profissionais.",
             link: "https://claude.ai/login?returnTo=%2F%3F"
         },
         {
             titulo: "Consensus",
-            imagem: "./assets-descubra/consensus.png",
+            imagem: "./assets-descubra/consensus.webp",
             descricao: "Consensus oferece resumos e respostas baseadas em consenso de múltiplas fontes, facilitando a pesquisa e análise.",
             link: "https://consensus.app"
         },
         {
             titulo: "Perplexity",
-            imagem: "./assets-descubra/perplexity.png",
+            imagem: "./assets-descubra/perplexity.webp",
             descricao: "Perplexity fornece respostas e análises detalhadas para questões acadêmicas, utilizando IA para interpretar dados e pesquisas.",
             link: "https://www.perplexity.ai"
         },
         {
             titulo: "Elicit",
-            imagem: "./assets-descubra/elicit.png",
+            imagem: "./assets-descubra/elicit.webp",
             descricao: "Elicit auxilia nos estudos e na organização de informações, otimizando o processo de coleta e análise de dados.",
             link: "https://elicit.com/welcome"
         },
